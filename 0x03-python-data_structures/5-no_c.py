ed820b404e8566cebdddef8e820a6f75aed83fdf
bismarkb609 <bismarkb609@gmail.com>
2022-06-06 20:20:04 -0700

    remaining task

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for j in my_string:
+        if i is not 'c' and i is not 'C':
+            new_string += j
+    return (new_string)

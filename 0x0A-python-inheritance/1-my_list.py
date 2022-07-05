df6cc5704eafc7a0cac1ae8f879ee4e24a4d279d
bismarkb609 <bismarkb609@gmail.com>
2022-07-05 12:05:33 -0700

    change

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 """
-  1-my_list module
-  class Mylist that inherits from list
-  Public instance method : def print_sorted(self)
+    Module: 1-my_list
+
 """
 
 
 class MyList(list):
-    """ Contain a function with sorted list """
+    """
+        class MyList
+        methods:
+            print_sorted: prints a sorted list
+    """
 
     def print_sorted(self):
         """ Print the list in ascending order  """
+
         print(sorted(self))

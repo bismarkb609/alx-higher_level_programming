db1aa48ac10d47fdf23830b14bf4cdb8283b1202
bismarkb609 <bismarkb609@gmail.com>
2022-07-04 19:19:40 -0700

    task one

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Lookup Module """
+
+
+def lookup(obj):
+    """
+        Function returns all attributes and methods of an
+        object.
+    """
+
+    return vars(obj):

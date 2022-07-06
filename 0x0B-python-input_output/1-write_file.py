a858ffec916d866e2d2264d16bcd478d57be7fcc
bismarkb609 <bismarkb609@gmail.com>
2022-07-05 19:40:50 -0700

    added change

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,5 @@
 def write_file(filename="", text=""):
     """ Write a string to a file """
 
-    with open(filename, mode="w+", encoding="utf-8") as myFile:
+    with open(filename, mode="w", encoding="utf-8") as myFile:
         return myFile.write("This School is so cool!\n")

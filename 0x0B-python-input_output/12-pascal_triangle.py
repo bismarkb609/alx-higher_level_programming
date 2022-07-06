9092ddd579c7f02c37079f5f3b5568acefa9ca80
bismarkb609 <bismarkb609@gmail.com>
2022-07-05 19:56:28 -0700

    addded new change

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,29 +1,22 @@
 #!/usr/bin/python3
-""" Module : 11-student """
+"""12-pascal_triangle.py
+Defines a Pascal's Triangle function.
+"""
 
 
-class Student(object):
-    """A student class."""
+def pascal_triangle(n):
+    """Represent Pascal's Triangle of size n.
+    - Returns: a list of lists of integers representing the triangle.
+    """
+    if n <= 0:
+        return []
 
-    def __init__(self, first_name, last_name, age):
-        """Initialize student class."""
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of a Student instance."""
-        new_dict = {}
-        if (attrs is not None):
-            for i in attrs:
-                if i in self.__dict__:
-                    new_dict[i] = self.__dict__[i]
-            return new_dict
-        else:
-            return self.__dict__
-
-    def reload_from_json(self, json):
-        """Replaces all attributes of the Student instance."""
-        for i in self.__dict__:
-            if i in json:
-                self.__dict__[i] = json[i]
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles

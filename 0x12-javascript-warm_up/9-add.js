94bf321750f4d0d33766542e031b08fd11d792db
bismarkboateng <bismarkb609@gmail.com>
2022-09-05 17:29:39 -0700

    change

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,8 +2,8 @@
 
 const args = process.argv.slice(2);
 
-function add(a, b) {
-  console.log(Number(a) + Number(b))
+function add (a, b) {
+  console.log(Number(a) + Number(b));
 }
 
 add(args[0], args[1]);

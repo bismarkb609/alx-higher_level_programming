6b5d3b2c36b893dbef3ac2c999a85cc4c12c2988
bismarkboateng <bismarkb609@gmail.com>
2022-09-05 18:17:50 -0700

    increment

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const myObject = {
-	  type: 'object',
-	  value: 12
+  type: 'object',
+  value: 12
 };
 console.log(myObject);
 
-myObject.incr = function() {
+myObject.incr = function () {
   this.value++;
 };
 

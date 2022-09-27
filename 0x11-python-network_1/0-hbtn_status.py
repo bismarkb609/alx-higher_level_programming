033b0463ea8847b2252ba9fd60b687fa05430c30
bismarkboateng <bismarkb609@gmail.com>
2022-09-27 07:02:39 -0700

    taskt

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-""" Fetches https://intranet.hbtn.io/status """
-if __name__ == "__main__":
+""" fetches https://intranet.hbtn.io/status """
+if __name__ == '__main__':
     import urllib.request
-    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
-        html = response.read()
-        print("Body response:$")
-        print(f"\t- type: {type(html)}$")
-        print(f"\t- content: {format(html)}$")
-        print(f"\t- utf8 content: {format(html.decode('utf-8', 'replace'))}i$")
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as \
+            response:
+        url = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(url)))
+        print("\t- content: {}".format(url))
+        print("\t- utf8 content: {}".format(url.decode('utf-8')))

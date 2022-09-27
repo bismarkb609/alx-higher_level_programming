ab9f2963a5b1a1eba8287e9bdb320a84d916cbc4
bismarkboateng <bismarkb609@gmail.com>
2022-09-27 06:12:01 -0700

    task1

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+#Fetches https://intranet.hbtn.io/statu
+
+if __name__ == "__main__":
+    import urllib.request 
+
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+        html = response.read()
+
+        print("Body response:")
+        print(f"\t- type: {type(html)}")
+        print(f"\t- content: {format(html)}")
+        print(f"\t- utf8 content: {format(html.decode('utf-8', 'replace'))}")
+
+
+

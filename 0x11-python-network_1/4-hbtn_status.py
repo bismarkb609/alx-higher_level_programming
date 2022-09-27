0b658b3ffb4faf33b7ef2100390813d2b5fcdba4
bismarkboateng <bismarkb609@gmail.com>
2022-09-27 08:24:38 -0700

    task4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""script that fetches https://alx-intranet.hbtn.io/status"""
+if __name__ == "__main__":
+    import requests
+
+    res = requests.get('https://intranet.hbtn.io/staus')
+    print('Body response:')
+    print('\t- type: {}'.format(type(res.text)))
+    print('\t- content: {}'.format(res.text))

a485a8384bbb4d5e97006832af9d1a69934a21ed
bismarkboateng <bismarkb609@gmail.com>
2022-09-27 06:55:39 -0700

    change

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,7 +2,7 @@
 """ Fetches https://intranet.hbtn.io/status """
 
 if __name__ == "__main__":
-    import urllib.request 
+    import urllib.request
 
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         html = response.read()
@@ -11,6 +11,3 @@ if __name__ == "__main__":
         print(f"\t- type: {type(html)}$")
         print(f"\t- content: {format(html)}$")
         print(f"\t- utf8 content: {format(html.decode('utf-8', 'replace'))}i$")
-
-
-

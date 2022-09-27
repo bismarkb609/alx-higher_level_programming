3b436ae1822482c720f4f6f0aa41436d4b650800
bismarkboateng <bismarkb609@gmail.com>
2022-09-27 07:27:24 -0700

    task1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-""" Takes in a url and email, send a post request to the email """
+""" Sends request to a url and display X-Request-Id """
 if __name__ == "__main__":
     import urllib.request
-    import urllib.parse
     import sys
 
-    value = {'email': sys.argv[2]}
-    data = urllib.parse.urlencode(value)
-    data = data.encode('ascii')
-    req = urllib.request.Request(sys.argv[1], data)
-    with urllib.request.urlopen(req) as response:
-        print(response.read().decode("utf-8", "replace"))
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.getheader("X-Request-ID"))

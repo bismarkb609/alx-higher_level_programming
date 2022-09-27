52c9e9b2ca416ea88441ec7bb3c5733c68fd0478
bismarkboateng <bismarkb609@gmail.com>
2022-09-27 08:29:36 -0700

    change

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,7 +3,7 @@
 if __name__ == "__main__":
     import requests
 
-    res = requests.get('https://intranet.hbtn.io/staus')
+    res = requests.get('https://intranet.hbtn.io/status')
     print('Body response:')
     print('\t- type: {}'.format(type(res.text)))
     print('\t- content: {}'.format(res.text))

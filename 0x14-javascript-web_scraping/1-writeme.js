89bf3a97a9aec3f9d5e69340b3251f166b0ef3fe
bismarkboateng <bismarkb609@gmail.com>
2022-10-18 21:53:32 +0000

    write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,4 +5,8 @@ const fs = require('fs');
 const file = process.argv[2];
 const data = process.argv[3];
 
-fs.writeFile(file, data, 'utf8');
+fs.writeFile(file, data, 'utf8', function (error) {
+  if (error) {
+    console.log(error);
+  }
+});

2f91327b45812140960cde90c063c39aa77cf704
bismarkboateng <bismarkb609@gmail.com>
2022-10-18 22:05:42 +0000

    status

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,8 +5,8 @@ const url = process.argv[2];
 
 request(url, function (error, response, body) {
   if (error) {
-    console.log(error);
+    console.log('error:', error);
   } else {
-    console.log(`code: <${response && response.statusCode}>`);
+    console.log('code:', response && response.statusCode);
   }
 });

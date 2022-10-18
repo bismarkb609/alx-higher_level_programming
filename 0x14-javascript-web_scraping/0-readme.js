1cd68887d2ff8197b6eeafc90d3be72bdf0cbcc2
bismarkboateng <bismarkb609@gmail.com>
2022-10-18 21:43:14 +0000

    task0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const file = process.argv[2];
 
-fs.readMe(file, 'utf8', function (err, data) {
+fs.readFile(file, 'utf8', function (err, data) {
   console.error('error', err);
   console.log(data);
 });
